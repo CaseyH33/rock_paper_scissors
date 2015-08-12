1f6ddb6ee5834234ac7eb083c01ca06686f59f0f
Casey Heitz & Sam Martinez <student@epicodus.com>
2015-08-12 12:06:21 -0700

    comment out unneeded codeto check for errors

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -5,13 +5,13 @@
         function play_rockPaperScissors($user1, $user2)
         {
             //Converts user inputs to lowercase
-            $user1 = strtolower($user1);
+/*          $user1 = strtolower($user1);
             $user2 = strtolower($user2);
 
             // Verifies users input rock, paper, or scissors
             if((($user1 == "rock") || ($user1 == "paper") || ($user1 == "scissors")) &&
             (($user2 == "rock") || ($user2 == "paper") || ($user2 == "scissors")))
-            {
+            { */
                 //User 1 inputs rock and returns outcome dependent on User 2s input
                 if($user1 == "rock")
                 {
@@ -46,13 +46,13 @@
                         return "Player 2";
                     }
                 }
-            }
-
+        }
+/*
             //Returns an error message if either user inputs enter something besides
             //rock, paper, or scissors
             else {
                 return "Please input either 'Rock', 'Paper', or 'Scissors'";
             }
-        }
+        } */
     }
 ?>
