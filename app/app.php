97952dad9e20134124493d280ae3f677fa4e3d7b
Casey Heitz & Sam Martinez <student@epicodus.com>
2015-08-12 12:03:33 -0700

    Add form for user inputs

--- app/app.php
+++ app/app.php
@@ -4,7 +4,7 @@
     require_once __DIR__.'/../src/RockPaperScissors.php';
 
     $app = new Silex\Application();
-    $app['debug'] = true;
+    //$app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
@@ -20,5 +20,5 @@
     });
 
     return $app;
-    
+
  ?>
